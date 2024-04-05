c636ce7fc439b6108d1309a67782b48c8680b855
Will Lee <willericlee@gmail.com>
2024-04-04 20:29:24 -0700

    Make Todo, Project classes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
 import './style.css';
-import plus from './images/plus.svg';
+
+class Todo {
+    constructor (title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+class Project {
+    todos = [];
+
+    constructor (title, description) {
+        this.title = title;
+        this.description = description;
+    }
+}
+
+let projects = [];
+
