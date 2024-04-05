19dd93e1c9ca2dc92773f34f69ef77dea663d46f
Will Lee <willericlee@gmail.com>
2024-04-04 21:08:45 -0700

    Create example date/time object in console

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style.css';
+import { parse } from 'date-fns';
 
 class Todo {
     constructor (title, description, dueDate, priority) {
@@ -6,6 +7,10 @@ class Todo {
         this.description = description;
         this.dueDate = dueDate;
         this.priority = priority;
+    }
+
+    logItems() {
+        console.log(this.title, this.description, this.dueDate, this.priority);
     }
 }
 
@@ -19,4 +24,8 @@ class Project {
 }
 
 let projects = [];
+const exampleDateTime = '2022-06-01T17:31:02';
+const example = new Todo('Example', 'This is a description', parse(exampleDateTime, "yyyy-MM-dd'T'HH:mm:ss", new Date()), 'priority wawa')
 
+projects.push(example);
+projects[0].logItems();
